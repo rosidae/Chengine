635f01468b6b0a22ea5159dce6df561bd334f880
rosidae <windows10vmstuff@gmail.com>
2022-07-10 03:54:51 -0500

    My brain hurts..

--- src/uci/ucii.hpp
+++ src/uci/ucii.hpp
@@ -1,4 +1,9 @@
 #ifndef INC_UCII
     #define INC_UCII
-
+#include <iostream>
+#include <string>
+#include "ucio.hpp"
+void parse(std::string cmd) {
+    // TODO
+}
 #endif
--- src/uci/uciloop.hpp
+++ src/uci/uciloop.hpp
@@ -5,17 +5,20 @@
 void uciloop() {
     std::string gui = "";
     bool uciok = false;
-    while(true) {
+    while(1) {
         std::cin >> gui;
         if(gui == "uci") {
             uprint("id name Chengine");
             uprint("id author Samuel K");
             uprint("uciok");
-            uciok=!uciok;
+            uciok = true;
         }
-        if(!uciok) {
-            break;
+        if(gui == "isready") {
+            if(uciok) uprint("readyok");
         }
-        
+        if(gui == "go depth 15") {
+            uprint("info score cp -95 depth 1 nodes 100000");
+            uprint("bestmove d7d5");
+        }
     }
 }
